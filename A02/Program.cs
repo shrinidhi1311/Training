97bbfd730c9295fdd5dc789aca97d369761c69e5
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-07-23 14:38:58 +0530

    A02:Guessing game

---
+++ A02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program {
+   static void Main () {
+      int n = new Random ().Next (1, 101);
+
+      Console.WriteLine ("Guess a number from 1 to 100:");
+      int guess = int.Parse (Console.ReadLine ());
+      while (guess != n) {
+         if ((guess < n)) {
+            Console.WriteLine ("too low! Try again.");
+         } else {
+            Console.WriteLine ("too high!Try again");
+         }
+         Console.WriteLine ("guess a number :");
+         guess = int.Parse (Console.ReadLine ());
+      }
+      Console.WriteLine ("Congrats! You guessed the number correctly");
+      Console.WriteLine ("Press any key to exit...");
+      Console.ReadKey ();
+   }
+}
