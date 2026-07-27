c4d370ca4cd5fa7051c622c2f854f9eb54109fb6
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-07-27 08:46:00 +0530

    A02:Limit guessing game to 7 attempts

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+
 class Program {
    static void Main () {
       int n = new Random ().Next (1, 101);
+      int attempts = 1;
 
       Console.WriteLine ("Guess a number from 1 to 100:");
       int guess = int.Parse (Console.ReadLine ());
+
       while (guess != n) {
-         if ((guess < n)) {
-            Console.WriteLine ("too low! Try again.");
+         if (guess < n) {
+            Console.WriteLine ("Too low! Try again.");
          } else {
-            Console.WriteLine ("too high!Try again");
+            Console.WriteLine ("Too high! try again.");
          }
-         Console.WriteLine ("guess a number :");
+
+         if (attempts >= 7) {
+            Console.WriteLine ("Too many attempts!");
+            Console.WriteLine ($"The correct number was {n}.");
+            Console.WriteLine ("Press any key to exit...");
+            Console.ReadKey ();
+            return;
+         }
+
+         Console.WriteLine ("Guess a number:");
          guess = int.Parse (Console.ReadLine ());
+         attempts++;
       }
-      Console.WriteLine ("Congrats! You guessed the number correctly");
+
+      Console.WriteLine ("Congrats! You guessed the number correctly!");
+      Console.WriteLine ($"You guessed it in {attempts} attempt(s).");
       Console.WriteLine ("Press any key to exit...");
       Console.ReadKey ();
    }
