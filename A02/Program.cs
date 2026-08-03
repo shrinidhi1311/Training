b253956136b494124f2a12bcff18f181b27c4d61
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-08-03 14:20:44 +0530

    Review changes:Refactored game logic

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,24 +1,36 @@
-﻿using static System.Console;
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Program to implement a number guessing game with input validation and a maximum of seven attempts.
+using static System.Console;
 class Program {
    static void Main () {
-      int n = new Random ().Next (1, 101), attempts = 1,guess;
-      Print ("Guess a number from 1 to 100:");
+      int n = new Random ().Next (1, 101), attempts = 1, guess;
+      string message = "Guess a number from 1 to 100:";
+      Print (message);
       while (attempts <= 7) {
          guess = ReadGuess ();
-         if (guess == n) break;
+         if (guess == n) {
+            message = $"You guessed it!\nAttempts: {attempts}\nPress any key...";
+            break;
+         } else if (attempts == 7) {
+            message = $"No attempts left!\nCorrect Number: {n}\nPress any key...";
+            break;
+         }
          Print ($"Too {(guess < n ? "low" : "high")}! Try again.\nGuess again:");
          attempts++;
       }
-      if (attempts <= 7) Print ($"You guessed it!\nAttempts: {attempts}\nPress any key...");
-      else Print ($"No attempts left!\nCorrect Number: {n}\nPress any key...");
+      Print (message);
       ReadKey ();
    }
 
    static int ReadGuess () {
       int guess;
-      while (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) Print ("Invalid input.Try Again:");
+      while (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) Print ("Invalid input.Try again:");
       return guess;
    }
 
-   static void Print (string message) => WriteLine (message);
+   static void Print (string text) => WriteLine (text);
 }
