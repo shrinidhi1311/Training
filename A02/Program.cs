665b6467f3c706ee3b94b6911af58a1c0346cebb
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-08-03 09:38:03 +0530

    Review changes:Limited attempts,refactored the logic

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,23 +1,22 @@
 ﻿using static System.Console;
 class Program {
    static void Main () {
-      int n = new Random ().Next (1, 101), attempts = 1, guess;
+      int n = new Random ().Next (1, 101), attempts = 1,guess;
       Print ("Guess a number from 1 to 100:");
-      guess = ReadGuess ();
-      while (guess != n) {
-         Print ($"Too {(guess < n ? "low" : "high")}! Try again.");
-         Print ("Guess the number:");
+      while (attempts <= 7) {
          guess = ReadGuess ();
+         if (guess == n) break;
+         Print ($"Too {(guess < n ? "low" : "high")}! Try again.\nGuess again:");
          attempts++;
       }
-      Print ($"Congrats on guessing the correct number!\nTotal attempts taken: {attempts}\nPress any key to exit.");
+      if (attempts <= 7) Print ($"You guessed it!\nAttempts: {attempts}\nPress any key...");
+      else Print ($"No attempts left!\nCorrect Number: {n}\nPress any key...");
       ReadKey ();
    }
 
    static int ReadGuess () {
       int guess;
-      while (!int.TryParse (ReadLine (), out guess))
-         Print ("Invalid input. Please enter a valid number.");
+      while (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) Print ("Invalid input.Try Again:");
       return guess;
    }
 
