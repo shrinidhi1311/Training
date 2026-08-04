fb23ffc5cdf46407ed39816a6ecae92738d126e2
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-08-04 14:40:01 +0530

    Review changes:cascaded ternary condition,reuse boolean variables

--- A02/Program.cs
+++ A02/Program.cs
@@ -9,28 +9,28 @@ class Program {
    static void Main () {
       int n = new Random ().Next (1, 101), attempts = 1, guess;
       const int MAX_ATTEMPTS = 7;
+      bool won, lastAttempt;
       string message = "Guess a number from 1 to 100:";
       Print (message);
-      while (attempts <= MAX_ATTEMPTS && ReadGuess (out guess)) {
-         bool won = guess == n, lastAttempt = attempts == MAX_ATTEMPTS;
-         if (won || lastAttempt) {
-            message = won ? $"You guessed it!\nAttempts: {attempts}\nPress any key..."
-               : $"No attempts left!\nCorrect Number: {n}\nPress any key...";
-            break;
-         }
-         Print ($"Too {(guess < n ? "low" : "high")}! Try again.\nGuess again:");
+      while (ReadGuess (out guess)) {
+         won = guess == n;
+         lastAttempt = attempts == MAX_ATTEMPTS;
+         message = won ? $"You guessed it!\nAttempts: {attempts}\nPress any key..."
+            : lastAttempt ? $"No attempts left!\nCorrect Number: {n}\nPress any key..."
+            : $"Too {(guess < n ? "low" : "high")}! Try again.\nGuess again:";
+         Print (message);
+         if (won || lastAttempt) break;
          attempts++;
       }
-      Print (message);
       ReadKey ();
    }
 
-   // Reads and validates the user's guess.
+   // Reads and validates the user's guess
    static bool ReadGuess (out int guess) {
-      while (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) Print ("Invalid input.Try again:");
+      while (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) Print ("Invalid input. Try again:");
       return true;
    }
 
-   // Displays a message on the console.
+   // Displays a message on the console
    static void Print (string text) => WriteLine (text);
 }
