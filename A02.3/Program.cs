a05ee474b3ce637eee150fb658b67c5cb3fe37f7
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-08-17 14:53:19 +0530

    Review fix: Built binary representation using StringBuilder and converted to integer

--- A02.3/Program.cs
+++ A02.3/Program.cs
@@ -5,26 +5,31 @@
 // Program.cs
 // Program to determine the user's number using binary representation.
 using static System.Console;
+using System.Text;
 
 class Program {
    static void Main () {
-      Write ("Think of a number between 1 and 127." + "\nAnswer each question with Y or N.");
-      int number = 0, divisor = 1;
-      string prompt;
-      while (divisor <= 64) {
-         prompt = $"\nWhen the number is divided by {divisor * 2,3}, Is the remainder less than {divisor,2}? (Y/N): ";
-         number += GetResponse (prompt) * divisor;
-         divisor *= 2;
+      Write ("Think of a number between 1 and 127.\nAnswer each question with Y or N.");
+      int remainder = 0, bit, number, bitValue = 1;
+      StringBuilder binary = new ();
+      while (bitValue <= 64) {
+         bit = GetResponse ($"\nWhen the number is divided by {bitValue * 2, 3}, " +
+                            $"is the remainder {remainder, 3}? (Y/N): ");
+         binary.Insert (0, bit.ToString ());
+         remainder += bit * bitValue;
+         bitValue *= 2;
       }
+      number = Convert.ToInt32 (binary.ToString (), 2);
       WriteLine ($"\nYour number is {number}.");
    }
 
-   // Displays the prompt, reads and validates the user's response, and returns the corresponding bit
+   // Displays the prompt, reads and validates the user's response, and returns the corresponding binary bit
    static int GetResponse (string prompt) {
       Write (prompt);
       ConsoleKey key = 0;
-      while (!(key is ConsoleKey.Y or ConsoleKey.N)) key = ReadKey (true).Key;
+      while (!(key is ConsoleKey.Y or ConsoleKey.N))
+         key = ReadKey (true).Key;
       Write (key);
-      return key == ConsoleKey.N ? 1 : 0;
+      return key == ConsoleKey.Y ? 0 : 1;
    }
 }
