48d972306348de2431c9a496e98e0e96e42c0930
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-08-17 15:25:15 +0530

    Review fix: Appropriate naming of the variables

--- A02.3/Program.cs
+++ A02.3/Program.cs
@@ -4,22 +4,22 @@
 // ------------------------------------------------------------------------------------------------
 // Program.cs
 // Program to determine the user's number using binary representation.
-using static System.Console;
 using System.Text;
+using static System.Console;
 
 class Program {
    static void Main () {
       Write ("Think of a number between 1 and 127.\nAnswer each question with Y or N.");
-      int remainder = 0, bit, number, bitValue = 1;
-      StringBuilder binary = new ();
-      while (bitValue <= 64) {
-         bit = GetResponse ($"\nWhen the number is divided by {bitValue * 2, 3}, " +
+      int remainder = 0, divisor = 1, bit, number;
+      StringBuilder sb = new ();
+      while (divisor <= 64) {
+         bit = GetResponse ($"\nWhen the number is divided by {divisor * 2, 3}, " +
                             $"is the remainder {remainder, 3}? (Y/N): ");
-         binary.Insert (0, bit.ToString ());
-         remainder += bit * bitValue;
-         bitValue *= 2;
+         sb.Insert (0, bit);
+         remainder += bit * divisor;
+         divisor *= 2;
       }
-      number = Convert.ToInt32 (binary.ToString (), 2);
+      number = Convert.ToInt32 (sb.ToString (), 2);
       WriteLine ($"\nYour number is {number}.");
    }
 
@@ -27,8 +27,7 @@ class Program {
    static int GetResponse (string prompt) {
       Write (prompt);
       ConsoleKey key = 0;
-      while (!(key is ConsoleKey.Y or ConsoleKey.N))
-         key = ReadKey (true).Key;
+      while (!(key is ConsoleKey.Y or ConsoleKey.N)) key = ReadKey (true).Key;
       Write (key);
       return key == ConsoleKey.Y ? 0 : 1;
    }
